0498dea4ef55a8f87f1ea21dd4393774d2d19992
Umair Ahmad <umairshams65@gmail.com>
2017-01-18 21:56:08 +0500

    Completed Ui for News Feed

--- Social_Network/CircleView.swift
+++ Social_Network/CircleView.swift
@@ -1,14 +1,14 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  Social_Network
 //
-//  Created by Umair Ahmad on 16/01/2017.
+//  Created by Umair Ahmad on 18/01/2017.
 //  Copyright © 2017 Umair Ahmad. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +17,13 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+        
     }
 
 }
